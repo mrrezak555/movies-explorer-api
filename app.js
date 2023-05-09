7baa0bce270c49a1b1581df10972cae5c2c9f74b
Kirill Zuev <mrrezak555@yandex.ru>
2023-05-09 11:38:02 +0300

    lev_1_3

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const cors = require('./middlewares/cros');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { router } = require('./routes/index');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const INTERNAL_ERROR = 500;
 
-mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+mongoose.connect(DB_ADDRESS, {
   useNewUrlParser: true,
 });
 
--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,8 @@ const {
 router.get('/me', getCurrentUser);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email(),
-    name: Joi.string().min(2).max(30),
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
 
